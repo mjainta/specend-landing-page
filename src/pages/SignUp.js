05d54d699f73255c4ac1f75323297a1f9af1ec8b
mjainta <martin.jainta@googlemail.com>
2021-07-24 20:48:55 +0200

    :bug: Fix netlify form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -24,6 +24,7 @@ function SignUp() {
               {/* Form */}
               <div className="max-w-sm mx-auto">
                 <form name="subscribe" method="POST" data-netlify="true">
+                  <input type="hidden" name="form-name" value="subscribe" />
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
                       <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
