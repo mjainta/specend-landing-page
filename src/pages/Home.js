455beaf49bee3ff4023c0c485268b3c879ed4d42
mjainta <martin.jainta@googlemail.com>
2021-07-24 10:51:55 +0200

    :sparkles: Add first version cookie consent

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,29 @@ import FeaturesBlocks from '../partials/FeaturesBlocks';
 import Newsletter from '../partials/Newsletter';
 import Footer from '../partials/Footer';
 
+import CookieConsent from "react-cookie-consent";
+import { Link } from 'react-router-dom';
+
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
+      <CookieConsent
+        location="bottom"
+        buttonText="I accept all cookies"
+        enableDeclineButton
+        declineButtonText="I decline all cookies"
+        cookieName="google_analytics"
+        buttonStyle={{ color: "#fff", background: "#428061"}}
+        buttonClasses="text-sm"
+        declineButtonClasses="text-sm"
+        expires={150}
+        onAccept={() => {
+        }}
+        onDecline={() => {}}
+      >
+        This website uses cookies and Google Analytics to enhance the user experience.{" "}
+        View the <Link to="/privacypolicy" className="text-teal-300 underline hover:text-teal-600 transition duration-150 ease-in-out">Privacy Policy</Link> for more information.
+      </CookieConsent>
 
       {/*  Site header */}
       <Header />
