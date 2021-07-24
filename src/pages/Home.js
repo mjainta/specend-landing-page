7c3647c13c0efe1ce1d7ee20ff84a462d1ac2d89
mjainta <martin.jainta@googlemail.com>
2021-07-24 20:00:35 +0200

    :sparkles: Control ga with the cookie consent

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Header from '../partials/Header';
 import HeroHome from '../partials/HeroHome';
@@ -6,11 +6,32 @@ import FeaturesHome from '../partials/Features';
 import FeaturesBlocks from '../partials/FeaturesBlocks';
 import Newsletter from '../partials/Newsletter';
 import Footer from '../partials/Footer';
+import { initGA } from "../utils/ga";
 
-import CookieConsent from "react-cookie-consent";
+import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-consent";
 import { Link } from 'react-router-dom';
 
 function Home() {
+  const handleAcceptCookie = () => {
+    if (process.env.REACT_APP_GA_CODE) {
+      initGA(process.env.REACT_APP_GA_CODE);
+    }
+  };
+
+  const handleDeclineCookie = () => {
+    //remove google analytics cookies
+    Cookies.remove("_ga");
+    Cookies.remove("_gat");
+    Cookies.remove("_gid");
+  };
+
+  useEffect(() => {
+    const isConsent = getCookieConsentValue();
+    if (isConsent === "true") {
+      handleAcceptCookie();
+    }
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
       <CookieConsent
@@ -23,9 +44,8 @@ function Home() {
         buttonClasses="text-sm"
         declineButtonClasses="text-sm"
         expires={150}
-        onAccept={() => {
-        }}
-        onDecline={() => {}}
+        onAccept={handleAcceptCookie}
+        onDecline={handleDeclineCookie}
       >
         This website uses cookies and Google Analytics to enhance the user experience.{" "}
         View the <Link to="/privacypolicy" className="text-teal-300 underline hover:text-teal-600 transition duration-150 ease-in-out">Privacy Policy</Link> for more information.
