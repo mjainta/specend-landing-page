2a917549101c379933ce3c26cc8370e33e9cde69
mjainta <martin.jainta@googlemail.com>
2021-08-02 19:27:35 +0200

    :art: Make email input required to eliminate empty submissions

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -28,7 +28,7 @@ function SignUp() {
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
                       <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
-                      <input id="email" name="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
+                      <input id="email" required name="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
                     </div>
                   </div>
                   <div className="flex flex-wrap -mx-3 mt-6">
